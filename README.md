f3952101d3259a1bb891bd536cb486b645a7275c
Andrei Belov <abelov@student.42london.com>
2024-05-18 08:10:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# FdF
+## A project about making a wireframe and projection rendering.
+
+This project is about creating a simple wireframe model representation of a 3D
+landscape by linking various points (x, y, z) with line segments (edges).
+
+### Objectives:
+
+To get familiar with the [MiniLibX](https://github.com/42Paris/minilibx-linux) library and discover the basics of graphics programming.
+
+
+![Screenshot of a working programm](/resources/program_screenshot.png)
---
+++ src/ft_fdf.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fdf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abelov <abelov@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/18 07:37:23 by abelov            #+#    #+#             */
+/*   Updated: 2024/05/18 07:37:24 by abelov           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_FDF_H
+# define FT_FDF_H
+# include <stdlib.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include "ft/get_next_line.h"
+# include "ft/libft.h"
+# include "mlx.h"
+
+
+
+#endif //FT_FDF_H
